354da7dd7b1c9a2edc7e94f2f958ce55a252bc90
y.kimura <y.kimura@simount.com>
2025-09-24 14:39:44 +0900

    modify config

---
+++ lua/option.lua
@@ -0,0 +1,5 @@
+vim.opt.termguicolors = true
+vim.opt.winblend = 0 -- ウィンドウの不透明度
+vim.opt.pumblend = 0 -- ポップアップメニューの不透明度
+
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      -- ここに diffview.nvim の設定を記述します
+      -- 例えば、特定のキーマップを設定できます
+    },
+  },
+}
